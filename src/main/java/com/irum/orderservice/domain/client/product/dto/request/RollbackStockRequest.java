f5a1b6cb357223145b3c645c817f411b35dd9a28
hyeonji91 <hjkim1770@gmail.com>
2025-11-07 14:34:00 +0900

    feature #12 : rollback feign 추가

---
+++ src/main/java/com/irum/orderservice/domain/client/product/dto/request/RollbackStockRequest.java
@@ -0,0 +1,13 @@
+package com.irum.orderservice.domain.client.product.dto.request;
+
+
+import lombok.Builder;
+
+import java.util.List;
+import java.util.UUID;
+
+@Builder
+public record RollbackStockRequest(List<OptionValueRequest> optionValueList) {
+    @Builder
+    public record OptionValueRequest(UUID optionValueId, int quantity) {}
+}
