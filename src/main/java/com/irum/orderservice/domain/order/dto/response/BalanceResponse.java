b3e0f41ba360e723dd2aaabb21467a4c9fa5408e
moses249 <ghdahtp15@gmail.com>
2025-11-10 17:28:21 +0900

    feature #3: PR 리뷰 반영

--- src/main/java/com/irum/orderservice/domain/order/dto/response/BalanceResponse.java
+++ src/main/java/com/irum/orderservice/domain/order/dto/response/BalanceResponse.java
@@ -4,4 +4,4 @@ public record BalanceResponse(
         int totalSalesAmount, // 총 결제 금액 합계
         int totalRefundAmount, // 총 환불 금액 합계
         int settlementAmount // 정산 금액 (총 금액 - 환불 금액)
-) {}
+        ) {}
