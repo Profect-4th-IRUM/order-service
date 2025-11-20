4e7666cee107e923f5a58efdf04d3cc75ca28a3c
willjsw <sunwoo1137@gmail.com>
2025-11-20 21:13:22 +0900

    hotfix: h2 liquibase disabled

--- src/test/java/com/irum/orderservice/OrderServiceApplicationTest.java
+++ src/test/java/com/irum/orderservice/OrderServiceApplicationTest.java
@@ -2,8 +2,10 @@ package com.irum.orderservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class OrderServiceApplicationTest {
 
     @Test
